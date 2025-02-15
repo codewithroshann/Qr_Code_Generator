8d5469bf242a6b89358965479eb2c011043099be
roshan sahu <locroshan@gmail.com>
2025-02-15 13:49:24 +0530

    some chnages on vercel.json

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // server.js (Backend - Express.js)
 const express = require('express');
-const QRCode = require('qrcode')
+const QRCode = require('qrcode');
 const crypto = require("crypto");
-const fileSchema = require('./models/fileModel')
+const fileSchema = require('./models/fileModel');
 const path = require('path');
 const ejs = require('ejs'); // For EJS templating
 const app = express();
-const multer = require('multer')
-const dotenv = require('dotenv')
+const multer = require('multer');
+const dotenv = require('dotenv');
+const fs = require('fs');
 dotenv.config()
 const port = process.env.PORT ||5000;
 
@@ -35,7 +36,7 @@ app.get('/', async (req, res) => {
 });
 
 app.post('/generate', upload.single("file"), async (req, res) => {
-    try {
+     try {
         let qrCodeFile;
         let qrCodeText;
         if (req.file) {
@@ -43,23 +44,18 @@ app.post('/generate', upload.single("file"), async (req, res) => {
                 filename: req.file.filename,
                 path: `../files/uploads/${req.file.filename}`,
                 contentType: req.file.mimetype,
-            });
-            console.log(newFile)
+            });          
             await newFile.save();
-
             const fileUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
             qrCodeFile = fileUrl;
         }
-        else if (req.body.text) { // File upload
-            // qrCodeFile = req.file ? req.file.path.slice(8, req.file.path.length) : null; // Base64 file content
-            qrCodeText = await req.body.text;
-
+        else if (req.body.text) { // text upload
+           qrCodeText = await req.body.text;
         } // Text input
         else {
             return res.status(400).send('No data provided');
         }
         const inputData = qrCodeFile ? qrCodeFile : qrCodeText;
-
         const qrCodeUrl = await QRCode.toDataURL(inputData);
        res.render('index', { qrCodeUrl }); // Re-render index.ejs with QR code URL
 
@@ -67,30 +63,29 @@ app.post('/generate', upload.single("file"), async (req, res) => {
         console.error("Error generating QR code:", error);
         res.status(500).send('Failed to generate QR code');
     }
+    setTimeout( async () => {
+        try{
+            fs.unlink(`${req.file.path}`,(err)=>{
+                console.log(err)
+            })
+            const file =  await fileSchema.findOneAndDelete({ filename: req.file.filename })
+            console.log(file,"file deleted")
+        }catch(err){
+            console.log("somthing went wrong")
+        }
+     }, 86400000);
+
 });
 
 
 app.get('/uploads/:filename', async (req, res) => {
     const file = await fileSchema.findOne({ filename: req.params.filename })
+    if (!file) {
+        return res.status(404).send('File not found');
+    }
    res.render('uploads', { file: file });
 
 })
-
-// Add this route to handle file downloads
-app.get('/download/:filename', (req, res) => {
-    const filename = req.params.filename;
-    const filePath = path.join(__dirname, 'uploads', filename); // Construct the full file path
-
-    res.download(filePath, filename, (err) => { // Use res.download to send the file
-        if (err) {
-            console.error("Error downloading file:", err);
-            if (err.code === 'ENOENT') {
-                return res.status(404).send("File not found.");
-            }
-            return res.status(500).send("Error downloading file.");
-        }
-    });
-});
 
 
 
