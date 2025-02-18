c9c25b6014b14610e2dc3eaf640a10606774bda3
roshan sahu <locroshan@gmail.com>
2025-02-18 11:49:12 +0530

    updated notification in datdbase file

--- models/fileModel.js
+++ models/fileModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 dotenv.config()
 mongoose.connect(process.env.MONGODB_URL)
-    .then(() => console.log('Connected!')).catch((err)=>{
+    .then(() => console.log('Connected successfully With Database!')).catch((err)=>{
         console.log(err)
     });
 
